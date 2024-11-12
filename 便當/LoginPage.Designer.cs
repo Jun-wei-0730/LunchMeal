62488deb8565eef4d3d01d337979bca2a72ffad8
Junwei <wl02812321@gmail.com>
2024-11-12 08:02:10 +0800

    加入專案檔案。

---
+++ 便當/LoginPage.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace 便當
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Test = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(211, 237);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 50);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Test
+            // 
+            this.Test.AutoSize = true;
+            this.Test.Font = new System.Drawing.Font("微軟正黑體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Test.Location = new System.Drawing.Point(227, 106);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(343, 47);
+            this.Test.TabIndex = 1;
+            this.Test.Text = "MealOrderSystem";
+            this.Test.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(288, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "請輸入使用者名字 :";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.loginButton.Location = new System.Drawing.Point(325, 306);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(153, 59);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "確認";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(800, 494);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Test);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Test;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ 便當/Menu.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace 便當
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BeginOrderButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BeginOrderButton
+            // 
+            this.BeginOrderButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BeginOrderButton.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BeginOrderButton.Location = new System.Drawing.Point(642, 398);
+            this.BeginOrderButton.Name = "BeginOrderButton";
+            this.BeginOrderButton.Size = new System.Drawing.Size(146, 49);
+            this.BeginOrderButton.TabIndex = 0;
+            this.BeginOrderButton.Text = "開始訂購";
+            this.BeginOrderButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
+            this.button1.Location = new System.Drawing.Point(747, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "登出";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(800, 494);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BeginOrderButton);
+            this.Name = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BeginOrderButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
